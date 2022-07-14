477f663d63157248ad74a008cdf7b530fead1cb1
Ubuntu <ksyrbaba0812@gmail.com>
2022-07-14 01:15:37 +0000

    ユーザーの更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -13,9 +17,19 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
-  def show
+
+  def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   private
