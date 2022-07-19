6a5504d3e363a4bfda990613d7e03b254a92120f
Ubuntu <ksyrbaba0812@gmail.com>
2022-07-19 02:44:37 +0000

    アカウント有効化のメール送信

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -13,8 +11,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
-  
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -24,7 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert is_logged_in?
+    # assert_template 'users/show'
+    # assert is_logged_in?
   end
 end
